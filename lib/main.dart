cb24b2bbe86d4e10db885fb1ed8aab0426ce3cd7
MuhammadSiddique119 <mohammadsiddiquefaraz@gmail.com>
2023-10-15 14:00:17 +0500

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/firstscreen.dart';
 
+
 void main() {
 
 runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home:firstscreen(),
     );
